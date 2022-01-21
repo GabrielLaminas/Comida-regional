66d98eb933741a30f46828db1cebfe5242b94254
Gabriel de Freitas Laminas <laminas533@gmail.com>
2022-01-21 18:15:09 -0300

    finalizando responsivo e add js

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,21 @@
 const btnMenu = document.querySelector('.nav--botaomobile');
+const navLink = document.querySelector('.menu__nav__link');
+const main = document.querySelector('#principal');
 
 function handleClick({target}){
   target.classList.toggle('ativo');
+  navLink.classList.toggle('ativo');
+  document.body.classList.toggle('ativo');
+  main.classList.toggle('ativo')
 }
 
 window.addEventListener('resize', function(e) {
   let tamanhoWidth = e.target.innerWidth;
   if(tamanhoWidth > 768){
     btnMenu.classList.remove('ativo');
+    navLink.classList.remove('ativo');
+    document.body.classList.remove('ativo');
+    main.classList.remove('ativo')
   }
 });
 
